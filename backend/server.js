aeaf7ccd24b48b45056f9cbbf3a4308417a476bb
elzaameti <ea65085@ubt-uni.net>
2025-05-22 20:01:11 +0200

    Implementation of notifications CRUD backend with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,25 @@ const http = require("http");
 const socketIo = require("socket.io");  
 const app = express();
 require('dotenv').config(); 
+
 require('./config/db.js');
 const connectMongo = require('./config/mongo.js');
-const cors = require('cors');
 connectMongo();
+
+const cors = require('cors');
 
 const userRoutes = require('./users/user.routes.js');
 const eventRoutes = require('./events/event.routes');
 const questionRoutes = require('./questions/routes/question.routes');
 const chatRoutes = require('./livechat/chat.routes.js');
-
 const categoryRoutes = require('./categories/category.routes');
+
+const notificationRoutes = require('./notifications/notification.routes.js');
+
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -36,8 +40,14 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 }); 
 
+// 📦 Routes
 app.use('/api/users', userRoutes);
 app.use('/api/events', eventRoutes);
+app.use('/api/questions', questionRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/categories', categoryRoutes);
+
+app.use('/api/notifications', notificationRoutes);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
