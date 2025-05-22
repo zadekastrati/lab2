eb7f1031d73883661f88555d01f4b48abc6b9a39
elzaameti <ea65085@ubt-uni.net>
2025-05-22 20:25:50 +0200

    resolving conflicts in server.js file removing duplicates

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-const http = require("http"); 
-const socketIo = require("socket.io");  
-const app = express();
-require('dotenv').config(); 
-
-require('./config/db.js');
-const connectMongo = require('./config/mongo.js');
-connectMongo();
-
-const cors = require('cors');
-
-const userRoutes = require('./users/user.routes.js');
-const eventRoutes = require('./events/event.routes');
-const questionRoutes = require('./questions/routes/question.routes');
-const chatRoutes = require('./livechat/chat.routes.js');
-const categoryRoutes = require('./categories/category.routes');
-
-const notificationRoutes = require('./notifications/notification.routes.js');
-
-const PORT = process.env.PORT || 5000;
-
-const corsOptions = {
-  origin: 'http://localhost:5000',
-=======
 const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
@@ -35,18 +10,23 @@ require("./config/db.js");
 const connectMongo = require("./config/mongo.js");
 connectMongo();
 
-const userRoutes = require("./users/user.routes.js");
-const eventRoutes = require("./events/event.routes");
-const questionRoutes = require("./questions/routes/question.routes");
-const chatRoutes = require("./livechat/chat.routes.js");
-const categoryRoutes = require("./categories/category.routes");
+
+
+const userRoutes = require('./users/user.routes.js');
+const eventRoutes = require('./events/event.routes');
+const questionRoutes = require('./questions/routes/question.routes');
+const chatRoutes = require('./livechat/chat.routes.js');
+const categoryRoutes = require('./categories/category.routes');
+
+const notificationRoutes = require('./notifications/notification.routes.js');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
->>>>>>> 11672c1ac9190917f70981c9951eba0e8300595d
+  origin: 'http://localhost:3000',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -62,16 +42,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); 
 
-app.use("/api/users", userRoutes);
-app.use("/api/events", eventRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/questions", questionRoutes);
-app.use("/api/chat", chatRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-<<<<<<< HEAD
-}); 
 
 // 📦 Routes
 app.use('/api/users', userRoutes);
@@ -81,9 +52,7 @@ app.use('/api/chat', chatRoutes);
 app.use('/api/categories', categoryRoutes);
 
 app.use('/api/notifications', notificationRoutes);
-=======
-});
->>>>>>> 11672c1ac9190917f70981c9951eba0e8300595d
+;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
